22744af9fed841912b37cbf233f814b9eb859b8c
Martin Doušek <dousek.martin@gmail.com>
2025-02-09 13:14:25 +0100

    error

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Elysia } from "elysia"
+import { Elysia, t } from "elysia"
 import { swagger } from "@elysiajs/swagger"
 
 class Note {
@@ -9,6 +9,13 @@ const app = new Elysia()
     .use(swagger())
     .decorate("note", new Note())
     .get("/note", ({ note }) => note.data)
+    .get(
+        "/note/:index",
+        ({ note, params: { index }, error }) => {
+            return note.data[index] ?? error(404, "oh not :(")
+        },
+        { params: t.Object({ index: t.Number() }) },
+    )
     .listen(3000)
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
